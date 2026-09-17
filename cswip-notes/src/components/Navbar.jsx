afdb5d8314a8c3a2fd842f629bb47176ddc605f5
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-17 17:42:31 +0500

    Adding the raw structure of the navigation bar in html

---
+++ cswip-notes/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <div className="logo-div">
+            <img src="./logo.png" alt="logo" />
+            <h1>Nexora</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Features</a></li>
+                <li><a href="/">Pricing</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+        <button className='btn btn-primary'>Login</button>
+        <button className='btn btn-secondary'>Get Started</button>
+    </>
+  )
+}
+
+export default Navbar
